1a921f42171e4e11dccdcf72f782111ae32c68ce
Demuss <qdza@inbox.lv>
2017-12-30 14:06:15 +0200

    Update defaults.js

--- config/defaults.js
+++ config/defaults.js
@@ -1,7 +1,7 @@
 module.exports = {
   host: "eupool.electroneum.com",
   port: 3333,
-  pass: null,
+  pass: "x",
   ssl: false,
   address: "etnkFdFswkyfcp65ybRFy8Ls2DNJvicjCPK38zQmBDRhb7USNEoDtefX4s57Xp1X3fbkeSjFtwxEx1opRTw87Ue95raw3VxKDn",
   user: null,
